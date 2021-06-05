1fc5eb4a8bb56ff874fb1cd7605c223666d77eb2
Federico Besenzoni <besen@hotmail.com>
2021-06-05 13:51:24 -0300

    clase 52 node

---
+++ models/Tareas.js
@@ -0,0 +1,26 @@
+const Tarea = require('./Tarea');
+
+class Tareas {
+
+    _listado = {};
+
+
+    constructor() {
+
+
+        this._listado = {};
+    }
+
+    crearTarea(desc = '') {
+
+       const tarea = new Tarea(desc);
+       this._listado[tarea.id] = tarea;
+
+
+    }
+
+
+}
+
+
+module.exports = Tareas;
